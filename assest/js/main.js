b4cb932d448a76e967bb7a8eb5a3beb58ca2dd3d
SAMER MAJID AL-BAHABSA <62020378+Samer7777@users.noreply.github.com>
2022-02-24 16:21:16 +0200

    Add files via upload

---
+++ assest/js/main.js
@@ -0,0 +1,60 @@
+const header = document.querySelector("header");
+const sectionOne = document.querySelector(".header");
+
+const faders = document.querySelectorAll(".fade-in");
+const sliders = document.querySelectorAll(".slide-in");
+
+const sectionOneOptions = {
+  rootMargin: "-200px 0px 0px 0px",
+};
+
+const sectionOneObserver = new IntersectionObserver(function (
+  entries,
+  sectionOneObserver
+) {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) {
+      header.classList.add("nav-scrolled");
+    } else {
+      header.classList.remove("nav-scrolled");
+    }
+  });
+},
+sectionOneOptions);
+
+sectionOneObserver.observe(sectionOne);
+
+const appearOptions = {
+  threshold: 0,
+  rootMargin: "0px 0px -250px 0px",
+};
+
+const appearOnScroll = new IntersectionObserver(function (
+  entries,
+  appearOnScroll
+) {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) {
+      return;
+    } else {
+      entry.target.classList.add("appear");
+      appearOnScroll.unobserve(entry.target);
+    }
+  });
+},
+appearOptions);
+
+faders.forEach((fader) => {
+  appearOnScroll.observe(fader);
+});
+
+sliders.forEach((slider) => {
+  appearOnScroll.observe(slider);
+});
+
+/* mobile menu */
+$("#mobile_menu").click(function () {
+  $(".menu_links").toggleClass("show");
+  $("#mobile_menu").toggleClass("show");
+  $("#mobile_menu i").toggleClass("fa-xmark");
+});
